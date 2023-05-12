f7b5872a8c6d0cfd0038484c72244ad44f526709
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-05-12 15:40:55 -0400

    added desc. & btn & update

--- referral.php
+++ referral.php
@@ -4,7 +4,7 @@
 <head>
     <script src="dmxAppConnect/dmxAppConnect.js"></script>
     <meta charset="UTF-8">
-    <title>Media | Raging Wolf Solutions</title>
+    <title>Referral Rewards | Raging Wolf Solutions</title>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
@@ -46,7 +46,7 @@
                     <h1 class="text-uppercase text-yellow text-warning text-center"><i class="fas fa-comments-dollar"></i></h1>
                     <h5 class="text-uppercase text-yellow text-warning text-center">Referral Rewards</h5>
                     <h1 class="text-light text-left">Raging Wolf Solutions Offers Referral Rewards</h1>
-                    <h5 class="text-white-50 mb-3 lh-base fw-normal">Refer a customer to the 'PACK'. Once their first shipment is successfully delivered, you'll receive a $40 Visa Gift Card. Additionally, we'll make a charitable donation to the&nbsp;<span class="text-white-50 hover-cursor" dmx-on:click="browser1.goto('https://www.uhhospitals.org/rainbow')"><i>Rainbow Children's Hospital </i></span> on your behalf as a token of our appreciation.&nbsp;<span class="text-warning hover-cursor">Terms apply.</span></h5>
+                    <h5 class="text-white-50 mb-3 lh-base fw-normal">Refer a customer to the 'PACK'. Once their first shipment is successfully delivered, you'll receive a $40 Visa Gift Card. Additionally, we'll make a charitable donation to the&nbsp;<span class="text-white-50 hover-cursor" dmx-on:click="browser1.goto('https://www.uhhospitals.org/rainbow')"><i>Rainbow Children's Hospital </i></span> on your behalf as a token of our appreciation.&nbsp;<span class="text-warning hover-cursor" dmx-on:click="browser1.goto('referral_terms.php')">Terms apply.</span></h5>
 
                 </div>
 
@@ -82,15 +82,15 @@
                 <div class="col-12">
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="module-card border rounded-0 rounded-1 border-secondary col-12 mt-3 mb-3 pt-3 pb-3 ps-3 pe-3">
+                            <div class="module-card border rounded-0 rounded-1 border-secondary col-12 mt-3 mb-3 pt-3 pb-3 ps-3 pe-3" id="Referral_Form_Col">
 
                                 <h5 class="text-light mb-3 pt-2">Have someone in mind? Let us know.</h5>
 
 
                                 <form id="referral_form" method="post" action="https://usebasin.com/f/dd70a6a52004">
-                                    <div class="row justify-content-around gx-2">
-                                        <div class="col-lg-5"><input id="contact_email" name="email" type="text" class="form-control mb-2 me-3" placeholder="Your email address..." required="" data-rule-email=""></div>
-                                        <div class="col-lg-5"><input id="contact_message" name="message" type="text" class="form-control mb-2 me-3" placeholder="Tell us about your referral..." required=""></div>
+                                    <div class="row gx-2 justify-content-start">
+                                        <div class="col-lg-5 col-auto"><input id="contact_email" name="email" type="text" class="form-control mb-2 me-3" placeholder="Your email address..." required="" data-rule-email=""></div>
+                                        <div class="col-lg-5 col-auto"><input id="contact_message" name="message" type="text" class="form-control mb-2 me-3" placeholder="Tell us about your referral..." required=""></div>
                                         <div class="col-lg-auto"><button id="btn1" class="btn module-cta-btn btn text-nowrap fw-bold btn-outline-warning w-100 mb-2 pt-2 pb-2" dmx-on:click="" type="submit">Let Us Know&nbsp;<i class="fas fa-arrow-right"></i></button></div>
                                     </div>
                                 </form>
@@ -135,9 +135,31 @@
                     <h1 class="text-light text-left">Why Ship With Raging Wolf Solutions?</h1>
 
                     <p class="text-white-50 mb-4">When it comes to logistics and freight management, the stakes are high and the challenges are many. Our dedicated team is here to navigate the complexities of shipping, offering you an unmatched combination of reliability, expertise, and personalized service. From air and ground expedites to full truckloads, van partials, flatbeds, and straight trucks, we tailor our services to meet your needs. And we do it all while maintaining the highest levels of communication and service, because we believe in building long-lasting relationships with our customers.</p>
-                    <h5 class="text-light text-left">Reason to choose us #1</h5>
-                    <p class="text-white-50">Reason explanation paragraph number 1.</p>
+                    <h5 class="text-light text-left">Expertise in Freight Management:</h5>
+                    <p class="text-white-50">Raging Wolf Solutions employs a team of logistics experts who have extensive experience and knowledge in managing freight. We understand the intricacies of coordinating shipments and ensure your freight is handled expertly from pick-up to delivery.
+                    </p>
+                    <h5 class="text-light text-left">Diverse Shipping Options:</h5>
+                    <p class="text-white-50">With a wide array of shipping options, from air/ground expedites to full truckloads, van partials, flatbeds, and straight trucks, Raging Wolf offers tailored solutions to meet the unique needs of each customer. We are not just a service provider; we are a versatile logistics partner.</p>
+                    <h5 class="text-light text-left">Exceptional Communication:</h5>
+                    <p class="text-white-50">Our team at Raging Wolf prides itself on providing excellent communication throughout the shipping process. From initial coordination to final delivery, we keep you informed and take immediate action on any potential issues that may arise.</p>
+                    <h5 class="text-light text-left">High Service Levels:</h5>
+                    <p class="text-white-50">Unlike larger, impersonal carriers, Raging Wolf Solutions focuses on offering top-tier, personalized service. We take pride in going the extra mile to ensure our customers' needs are not just met, but exceeded.</p>
+                    <h5 class="text-light text-left">Strong Relationships:</h5>
+                    <p class="text-white-50">At Raging Wolf Solutions, we believe in building strong relationships with our customers. We see each customer as a partner and take a genuine interest in their business success, which drives us to provide the best possible service.</p>
+                    <h5 class="text-light text-left">Reliability:</h5>
 
+
+
+
+
+                    <p class="text-white-50 pb-2">Raging Wolf Solutions is known for its reliability. We understand how critical, timely and accurate deliveries are to your business, and we strive to maintain our reputation for reliable service.</p>
+
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col gx-5">
+                    <div class="d-flex flex-wrap"><button id="btn2" class="btn module-cta-btn btn text-nowrap fw-bold btn-warning mb-2 me-2 pt-2 pb-2 refer-button" dmx-on:click="browser1.goto('#Referral_Form_Col')">Refer&ensp;Someone</button><button id="btn3" class="btn module-cta-btn btn text-nowrap fw-bold btn-outline-warning mb-2 pt-2 pb-2 refer-button" data-bs-toggle="modal" data-bs-target="#quote_modal">start a quote</button></div>
                 </div>
             </div>
         </div>
