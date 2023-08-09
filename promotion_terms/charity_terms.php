787f75d9d77eb847995959e2d3e86811ac9e6290
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-08-09 16:25:30 -0400

    terms text update

--- promotion_terms/charity_terms.php
+++ promotion_terms/charity_terms.php
@@ -46,31 +46,31 @@
 
                     <h1 class="text-uppercase text-yellow text-warning text-center"><i class="fas fa-comments-dollar"></i></h1>
                     <h5 class="text-uppercase text-yellow text-warning text-center">Referral Rewards</h5>
-                    <h1 class="text-light text-left mb-4">Referral Rewards: Terms and Conditions</h1>
+                    <h1 class="text-light text-left mb-4">Giving Back To The Community: Terms and Conditions</h1>
 
                     <div class="d-flex overflow-auto mb-4" style="height: 400px;">
                         <p class="text-white-50 text-start">
-                            Limitation of Liability: Raging Wolf Solutions shall not be liable for any indirect, special, incidental, consequential, or punitive damages, including but not limited to loss of use, loss of profits, or loss of data, whether in an action in contract, tort (including but not limited to negligence), or otherwise, arising out of or in any way connected with the use of or inability to use this program or the materials therein or resulting from unauthorized access to or alteration of data.
+                            Disclaimer for Raging Wolf Solutions' Promotion:
                             <br /><br />
-                            No Warranties: Raging Wolf Solutions does not guarantee, represent, or warrant that your use of our service will be uninterrupted, timely, secure, or error-free. We do not warrant that the results that may be obtained from the use of the service will be accurate or reliable.
+                            Promotion Period: This promotion begins on August 7, 2023 and will end on December 31, 2023, or when our video collectively reaches 1 million views across Facebook, LinkedIn, and YouTube - whichever comes first.
                             <br /><br />
-                            Indemnification: By participating in the referral program, you agree to indemnify, defend, and hold harmless Raging Wolf Solutions and its officers, directors, employees, contractors, agents, licensors, service providers, subcontractors, and suppliers from and against any and all losses, liabilities, expenses, damages, and costs, including reasonable attorneys' fees and court costs, arising or resulting from your use of the service and any violation of these Terms of Service.
+                            View Counting: Views are counted as the total sum of views from Raging Wolf Solutions' official social media platforms on Facebook, LinkedIn, and YouTube. Only genuine and organic views will be considered. Raging Wolf Solutions reserves the right to validate the legitimacy of views.
                             <br /><br />
-                            Dispute Resolution: Any dispute or claim arising out of or in connection with this referral program shall be adjudicated in the state or federal courts located within the state where Raging Wolf Solutions is headquartered, and you consent to exclusive jurisdiction and venue in such courts.
+                            Donation: For every additional 100k views on our video during the promotion period, Raging Wolf Solutions commits to donate $500 to Rainbow Children's Hospital. Donations will be made in installments of $500 for each 100k view milestone achieved.
                             <br /><br />
-                            Severability: If any provision of these terms is found to be unlawful, void, or for any reason unenforceable, then that provision shall be deemed severable from these terms and shall not affect the validity and enforceability of any remaining provisions.
+                            Maximum Donation &amp; View Limit: The promotion will conclude once the video has reached a collective total of 1 million views across all platforms, or once a total donation amount of $5,000 has been reached, whichever comes first.
                             <br /><br />
-                            This offer is valid only for Raging Wolf Solutions customers and supporters who are legal residents of the United States, 18 years or older. Offer valid from May 15, 2023 until further notice. By participating in this program, you agree to these terms and conditions. Raging Wolf Solutions reserves the right to modify or terminate this program at any time without prior notice.
+                            Tampering &amp; Fraud: Any attempts to artificially inflate view counts, use bots, or other methods of manipulation will result in disqualification from the promotion.
                             <br /><br />
-                            To qualify for referral rewards, the referred customer must not be a current or past customer of Raging Wolf Solutions and must make a shipment through Raging Wolf Solutions within 90 days of referral. The referral rewards will be processed within 30 days of the referred customer's first shipment delivery.
+                            Transparency: Raging Wolf Solutions will post screenshots of our analytics on our official social media platforms as we reach each new viewer count milestone (every 100,000 views).
                             <br /><br />
-                            This promotion is void where prohibited by law. Participants are responsible for all applicable taxes, fees, and other charges related to the receipt and use of the referral rewards. Raging Wolf Solutions is not responsible for lost, stolen, or misdirected gift cards or donations.
+                            General: This promotion is in no way sponsored, endorsed, or administered by, or associated with, Facebook, LinkedIn, YouTube, or any other third-party platform on which it is promoted. Participants acknowledge that they are providing their information to Raging Wolf Solutions and not to these third-party platforms.
                         </p>
 
                     </div>
                     <div class="d-flex justify-content-between">
                         <h4 class="text-left text-uppercase text-white-50 hover-cursor" dmx-on:click="browser1.goto('index.php')"><i class="fas fa-arrow-left"></i>&nbsp;Go Home</h4>
-                        <h4 class="text-left text-warning text-uppercase hover-cursor" dmx-on:click="browser1.goto('referral.php')">Go To Program&nbsp;<i class="fas fa-arrow-right"></i></h4>
+                        <h4 class="text-left text-warning text-uppercase hover-cursor" dmx-on:click="browser1.goto('https://www.uhhospitals.org/rainbow/about/uh-rainbow-foundation')">Go To Program&nbsp;<i class="fas fa-arrow-right"></i></h4>
                     </div>
 
 
