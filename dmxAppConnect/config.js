e7b46d56850f7b30eb6aae2a18a1f8eebceb85f9
Johnathan <jbwierzbicki@gmail.com>
2024-02-06 19:09:23 -0500

    progress on tracking page connection to google maps api services

--- dmxAppConnect/config.js
+++ dmxAppConnect/config.js
@@ -9,6 +9,96 @@ dmx.config({
     "load_ref": {
       "meta": null,
       "outputType": "text"
-    }
+    },
+    "tracking_api": [
+      {
+        "type": "array",
+        "name": "data",
+        "sub": [
+          {
+            "type": "object",
+            "name": "attributes",
+            "sub": [
+              {
+                "type": "text",
+                "name": "type"
+              },
+              {
+                "type": "text",
+                "name": "url"
+              }
+            ]
+          },
+          {
+            "type": "text",
+            "name": "Load_Reference__c"
+          },
+          {
+            "type": "text",
+            "name": "FreightTM__Trailer__c"
+          },
+          {
+            "type": "text",
+            "name": "PO__c"
+          },
+          {
+            "type": "text",
+            "name": "Pickup_Name_Address__c"
+          },
+          {
+            "type": "text",
+            "name": "Delivery_Name_Address__c"
+          },
+          {
+            "type": "object",
+            "name": "Location__c",
+            "sub": [
+              {
+                "type": "number",
+                "name": "latitude"
+              },
+              {
+                "type": "number",
+                "name": "longitude"
+              }
+            ]
+          },
+          {
+            "type": "text",
+            "name": "Id"
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "name": "headers",
+        "sub": [
+          {
+            "type": "text",
+            "name": "cache-control"
+          },
+          {
+            "type": "text",
+            "name": "content-encoding"
+          },
+          {
+            "type": "text",
+            "name": "content-type"
+          },
+          {
+            "type": "text",
+            "name": "date"
+          },
+          {
+            "type": "text",
+            "name": "strict-transport-security"
+          },
+          {
+            "type": "text",
+            "name": "vary"
+          }
+        ]
+      }
+    ]
   }
 });
--- tracking-ph-3.php
+++ tracking-ph-3.php
@@ -14,12 +14,22 @@
     <link rel="stylesheet" href="dmxAppConnect/dmxAutocomplete/dmxAutocomplete.css" />
     <script src="dmxAppConnect/dmxAutocomplete/dmxAutocomplete.js" defer></script>
     <script src="dmxAppConnect/dmxStateManagement/dmxStateManagement.js" defer></script>
+
+    <!-- Google Maps API -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDLPyBKLy4MIp6P7FeJE1OjOXARChjgo7E"></script>
+    <script src="dmxAppConnect/dmxFormatter/dmxFormatter.js" defer></script>
 </head>
 
 <body is="dmx-app" id="trakingph3">
     <dmx-query-manager id="load_ref_param"></dmx-query-manager>
     <dmx-api-datasource id="tracking_api" is="dmx-fetch" dmx-bind:url="tracking_base_url.value+load_ref.value"></dmx-api-datasource>
     <dmx-value id="load_ref" dmx-bind:value="load_ref_param.data.load_ref"></dmx-value>
+    <dmx-value id="location_latitude" dmx-bind:value="tracking_api.data[0].Location__c.latitude"></dmx-value>
+    <dmx-value id="location_longitude" dmx-bind:value="tracking_api.data[0].Location__c.longitude"></dmx-value>
+    <dmx-value id="location_city"></dmx-value>
+    <dmx-value id="location_state"></dmx-value>
+    <dmx-value id="destination" dmx-bind:value="tracking_api.data[0].Delivery_Name_Address__c"></dmx-value>
+    <dmx-value id="distance_remaining"></dmx-value>
     <dmx-value id="tracking_base_url" dmx-bind:value="'https://staff.my.salesforce-sites.com/services/apexrest/LoadLocationCustomerView/'"></dmx-value>
     <main class="min-vh-100 bg-dark text-warning">
         <div class="container-xl">
@@ -47,8 +57,10 @@
                                     <td class="w-50 fw-bold text-secondary">To:&nbsp;<span dmx-text="tracking_api.data[0].Delivery_Name_Address__c"></span></td>
                                 </tr>
                                 <tr>
-                                    <th scope="row" class="w-50 text-secondary">Current Location:&nbsp;<span dmx-text="tracking_api.data[0].Location__c"></span></th>
-                                    <td class="w-50 fw-bold text-secondary">Approx. Miles Remaining:&nbsp;<span dmx-text="tracking_api.data[0].Distance"></span></td>
+                                    <th scope="row" class="w-50 text-secondary">Current Location:&nbsp;<span dmx-text="(location_city.value+', '+location_state.value)" dmx-show="location_state.value"></span>
+                                    </th>
+                                    <td class="w-50 fw-bold text-secondary">Approx. Miles Remaining:&nbsp;<span dmx-text="distance_remaining.value"></span>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -79,6 +91,93 @@
 
 
     </main>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            setTimeout(initMap, 2000);
+            setTimeout(calculateDistance, 2000);
+        });
+
+        async function initMap() {
+            var latitude = dmx.parse('location_latitude.value');
+            var longitude = dmx.parse('location_longitude.value');
+
+            var location = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+            var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+                zoom: 15,
+                center: location
+            });
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map
+            });
+
+            // New code to geocode the latitude and longitude
+            var geocoder = new google.maps.Geocoder();
+            geocodeLatLng(geocoder, location);
+        }
+
+        function calculateDistance() {
+            var origin = dmx.parse('location_latitude.value') + ',' + dmx.parse('location_longitude.value');
+            // Ensure the destination address is a single line by replacing newlines with spaces
+            var destination = dmx.parse('destination.value').replace(/\n/g, ' ');
+
+            var service = new google.maps.DistanceMatrixService();
+            service.getDistanceMatrix({
+                origins: [origin],
+                destinations: [destination], // Now passing the single-line address
+                travelMode: 'DRIVING',
+                unitSystem: google.maps.UnitSystem.IMPERIAL,
+            }, callback);
+
+            function callback(response, status) {
+                if (status == 'OK') {
+                    var results = response.rows[0].elements;
+                    var element = results[0];
+                    if (element.status === "OK") {
+                        var distanceText = element.distance.text; // The distance in miles
+
+                        dmx.parse('distance_remaining.setValue("' + distanceText + '")');
+                    } else {
+                        console.error('Distance calculation failed due to: ' + element.status);
+                    }
+                } else {
+                    console.error('Distance Matrix request failed due to: ' + status);
+                }
+            }
+        }
+
+        function geocodeLatLng(geocoder, location) {
+            geocoder.geocode({ 'location': location }, function (results, status) {
+                if (status === 'OK') {
+                    if (results[0]) {
+                        // Assuming you want the full address; adjust as needed
+                        var addressComponents = results[0].address_components;
+                        var city = "", state = "";
+                        addressComponents.forEach(component => {
+                            if (component.types.includes("locality")) {
+                                city = component.long_name;
+                            }
+                            if (component.types.includes("administrative_area_level_1")) {
+                                state = component.short_name;
+                            }
+                        });
+                        // Now you have city and state, you can display them as needed
+                        // For example, setting them in an element's text
+
+                        dmx.parse('location_city.setValue("' + city + '")');
+                        dmx.parse('location_state.setValue("' + state + '")');
+
+                    } else {
+                        window.alert('No results found');
+                    }
+                } else {
+                    window.alert('Geocoder failed due to: ' + status);
+                }
+            });
+        }
+    </script>
+
     <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
 </body>
 
