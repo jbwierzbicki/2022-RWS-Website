9150351849ea1d7d7b5d85d5f4121ceea1f41f8b
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-02-16 17:26:00 -0500

    updated-tile-size

--- component--promotions--termsgroup.php
+++ component--promotions--termsgroup.php
@@ -1,6 +1,6 @@
 <!-- Wappler include head-page="promotions.php" appConnect="local" is="dmx-app" bootstrap5="local" fontawesome_5="cdn" jquery_slim_35="local" -->
 <div class="row pt-2 justify-content-center">
-    <div class="col-lg-4 pt-2 pb-2 ps-4 pe-4">
+    <div class="col-lg-5 pt-2 pb-2 ps-4 pe-4">
         <div class="row border rounded rounded-2 border-light border-offwhite h-100">
             <div class="col promo-card pt-2">
                 <div class="d-flex flex-column hover-cursor" dmx-on:click="browser1.goto('referral.php')">
@@ -9,13 +9,13 @@
                 </div>
 
                 <div class="d-flex">
-                    <p class="text-white-50 text-center">Know someone in need of freight transportation? Send them our way! Tell your referral about Raging Wolf by sharing our services to your network. Make sure they mention your name or business!<span class="text-warning hover-cursor" dmx-on:click="browser1.goto('promotion_terms/referral_terms.php')">&nbsp;See Terms.</span></p>
+                    <p class="text-white-50 text-center">Know someone in need of freight transportation? Send them our way! Tell your referral about Raging Wolf by sharing our services to your network. Make sure they mention your name or business!<br><span class="text-warning hover-cursor" dmx-on:click="browser1.goto('promotion_terms/referral_terms.php')">&nbsp;See Terms.</span></p>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="col-lg-4 pt-2 pb-2 ps-4 pe-4">
+    <div class="col-lg-5 pt-2 pb-2 ps-4 pe-4">
         <div class="row h-100">
             <div class="border rounded rounded-2 border-light border-offwhite promo-card col pt-2">
                 <div class="d-flex flex-column hover-cursor" dmx-on:click="browser1.goto('charity.php')">
@@ -25,7 +25,7 @@
 
                 </div>
                 <div class="d-flex">
-                    <p class="text-white-50 text-center">Our team is giving back to our future - <span class="text-white-50 hover-cursor" dmx-on:click="browser1.goto('https://www.uhhospitals.org/rainbow')"><i>UH Rainbow Children's Hospital </i></span>- and we need your help! Support this great cause and promote Raging Wolf Solutions at the same time.<span class="text-warning hover-cursor" dmx-on:click="browser1.goto('promotion_terms/charity_terms.php')">&nbsp;See Terms.</span></p>
+                    <p class="text-white-50 text-center">Our team is giving back to our future - <span class="text-white-50 hover-cursor" dmx-on:click="browser1.goto('https://www.uhhospitals.org/rainbow')"><i>UH Rainbow Children's Hospital </i></span>- and we need your help! Support this great cause and promote Raging Wolf Solutions at the same time.<br><span class="text-warning hover-cursor" dmx-on:click="browser1.goto('promotion_terms/charity_terms.php')">&nbsp;See Terms.</span></p>
                 </div>
             </div>
         </div>
