b4e7af1f26ffb0c8ef978fff2ce0ac61cb5c277d
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-04-05 16:54:37 -0400

    startted checkout notice

---
+++ wolfsden-notice.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="dmxAppConnect/dmxBootstrap5Modal/dmxBootstrap5Modal.js" defer></script>
+</head>
+
+<body is="dmx-app" id="wolfsdenotice">
+    <div class="modal" id="modal1" is="dmx-bs5-modal" tabindex="-1">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-center">!! AN IMPORTANT NOTICE REGARDING RWS CHECKOUT PROCESS !!</h5>
+                </div>
+                <div class="modal-body text-center">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger w-100 fw-bold text-black" data-bs-dismiss="modal">Go Back</button>
+                    <button type="button" class="btn btn-warning w-100 fw-bold">Proceed with Checkout</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
