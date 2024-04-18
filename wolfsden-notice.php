408e79d9820c166e37282e9ceaad0aa782666724
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-04-18 17:41:58 -0400

    minor styling updates

--- wolfsden-notice.php
+++ wolfsden-notice.php
@@ -14,20 +14,17 @@
 </head>
 
 <body is="dmx-app" id="wolfsdenotice">
-    <div class="module-card border rounded-0 rounded-1 border-secondary mt-3 mb-3 ms-3 me-3 pt-3 pb-3 ps-3 pe-3 text-light w-50 col" id="notice" is="dmx-bs5-modal" tabindex="-1">
-        <div class="modal-dialog" role="document">
+    <div class="modal" id="notice" is="dmx-bs5-modal" tabindex="-1" id="modal1" is="dmx-bs5-modal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
-                <div class="module-card-header">
-                    <h4 class="modal-title mt-3 mb-3 text-center">!! AN IMPORTANT NOTICE REGARDING RWS CHECKOUT PROCESS !!</h4>
+                <div class="modal-header">
+                    <h4 class="modal-title mt-3 mb-3 text-center">!! AN IMPORTANT NOTICE REGARDING RWS CHECKOUT PROCESS !!</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <hr>
-                <div class="module-card-body text-center">
+                <div class="modal-body text-center">
                     <p class="mt-3 mb-3">Details of our checkout/purchase process goes here.</p>
                 </div>
-                <hr>
-                <div class="module-card-footer">
-                    <button type="button" class="btn btn-danger w-100 fw-bold text-black mb-1" data-bs-dismiss="modal" id="back">Go Back</button>
-                    <button type="button" class="btn btn-warning w-100 fw-bold mt-1" id="proceed" dmx-on:click="">Proceed with Checkout</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger w-100 fw-bold text-black mb-1" data-bs-dismiss="modal" id="back" dmx-on:click="browser1.goto('wolfsden-cart.php')">Go Back</button><button type="button" class="btn btn-warning w-100 fw-bold mt-1" id="proceed" dmx-on:click="">Proceed with Checkout</button>
                 </div>
             </div>
         </div>
