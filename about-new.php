1072e8b5a7d9190cc89844bc955ef9e477ba85b1
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-06-04 18:48:05 -0400

    FOOB day initial com pt2

--- about-new.php
+++ about-new.php
@@ -1,10 +1,98 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-<!-- New Wappler Page -->
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>N.F.O.O.B. Day</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/stylesheet.css" />
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer=""></script>
+    <script src="dmxAppConnect/dmxBackgroundVideo/dmxBackgroundVideo.js" defer=""></script>
+
+    <script src="dmxAppConnect/dmxBootstrap5Modal/dmxBootstrap5Modal.js" defer></script>
+    <link rel="icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="dmxAppConnect/dmxValidator/dmxValidator.css" />
+    <script src="dmxAppConnect/dmxValidator/dmxValidator.js" defer></script>
+    <script>
+        function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"663126372a6e2c01c71a0faf"})},
+document.head.appendChild(o)}initApollo();
+    </script>
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "lfqgizjy3r");
+    </script>
+    <script>
+        !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_8f6e0e1be3ac9f1e37dea684410f7c092849/sdk.js"),(document.body || document.head).appendChild(n)}();
+    </script>
 </head>
-<body>
+
+<body is="dmx-app" id="index" class="bg-dark">
+
+    <?php include 'navbar_mini'; ?>
+
+    <?php include 'navbar_main'; ?>
+
+    <section id="modules" class="border-top rounded-1 rounded-0 min-vh-75 border-secondary pb-3">
+        <img src="assets/images/section-modal-bg-top-stone.png" class="w-100 mb-2">
+        <div class="container modules-container mt-0 mb-5 pb-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-light text-center display-4 fw-bold"><u>Celebrate Tradition, Trust and Legacy with The 'Pack' on June 23rd!</u></h1>
+                </div>
+            </div>
+            <div class="row mt-3">
+
+                <div class="col">
+                    <div class="row">
+                        <div class="offset-1 col-12" id="landing_teaser">
+
+                            <a><img src="assets/images/OIP.jpg" width="100%" class="img-fluid mb-3"></a>
+                            <h1 class="text-light mt-3 mb-3 display-6">Spotlighting Local Business for Upcoming National Family Owned &amp; Operated Business Day</h1>
+                            <h3 class="text-light mt-3 fw-light">Sunday, June 23rd marks National Family Owned &amp; Opporated Business Day and Raging Wolf Solutions takes this oppprtunity to highlight why shippers should choose to work with a smaller, more personal business like Raging Wolf. We will post about the things that our team does for its customers, the value we offer compared to a faceless, mega corporation like TQL, CH Robinson, etc.</h3>
+                            <article class="text-light mt-3">Every year on June 23, we observe <b>National Family Owned &amp; Operated Businesses Day</b> as a tribute to the resilience and longevity of family-run businesses that have withstood the test of time, navigating through various market challenges and obstacles. These businesses often exude a sense of warmth, personal touch, and reliability that many find lacking in larger corporate entities. This day serves as a reminder for proprietors of family enterprises to honor the dedication and groundwork laid by their ancestors, upon which their current success is built. It's a day for the members of such businesses to come together, celebrate their heritage, and acknowledge the enduring nature of family-owned companies, which represent some of the earliest business models in history.</article>
+
+                            <article class="text-light mt-3">Join us in our engaging articles as we spotlight Raging Wolf Solutions’ commitment to <b>National Family Owned &amp; Operated Businesses Day</b>. Our story is one of community engagement and heartfelt support, showcasing the initiatives that allow us to contribute meaningfully to the community. As you read on, you’ll uncover the various ways we’re making a difference and how these efforts resonate with the values of this special day, and how you can help us continue the cause!</article>
+                            <article class="text-light mt-3">............................................................................................Teaser for next article.................................................................................................</article>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Make into an include component! -->
+                <div class="col-3 offset-1 me-5" id="article_list">
+                    <a><img src="assets/images/OIP%20%281%29.jpg" class="img-fluid mb-2"></a>
+                    <h5 class="text-light">Outstanding Service with Competitive Pricing (compared to rock-bottom pricing)</h5>
+                    <a><img src="assets/images/media/Image%206.webp" class="img-fluid mt-3 mb-2"></a>
+                    <h5 class="text-light">Family and fun meets small business</h5><a><img src="assets/images/media/Image%2019.webp" class="img-fluid mt-3 mb-2"></a>
+                    <h5 class="text-light">Interview with a Founder</h5><a><img src="assets/images/Football%20Header%20Icon.png" class="img-fluid mt-3 mb-2"></a>
+                    <h5 class="text-light">Customer Perks: Annual football pool promotion</h5><a><img src="assets/images/Wide_40oz_Mug_Promo_Referral-_1_.webp" class="img-fluid mt-3 mb-2"></a>
+                    <h5 class="text-light">Customer Perks: New, premium promo items</h5><a><img src="assets/images/UH_New-Rainbow-Babies-Children_CMYK-removebg-preview-white-text.png" class="mt-3 mb-2 img-fluid"></a>
+                    <h5 class="text-light">Partnering with UH Rainbow Children's Hospital for an Alpha Cause, while Supporting the Future!</h5>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
