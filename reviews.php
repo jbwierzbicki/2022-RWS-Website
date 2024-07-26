9746fff77d673854ba10b6f81901d3833fb1d68d
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-07-26 15:31:27 -0400

    reviews landing initial commit

---
+++ reviews.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="dmxAppConnect/dmxSwiper/dmxSwiper.js" defer></script>
+    <link rel="stylesheet" href="dmxAppConnect/dmxSwiper/swiper.min.css" />
+    <script src="dmxAppConnect/dmxSwiper/swiper.min.js" defer></script>
+</head>
+
+<body is="dmx-app" id>
+    <section>
+        <div class="container text-bg-dark">
+            <div class="row">
+                <div class="col">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
