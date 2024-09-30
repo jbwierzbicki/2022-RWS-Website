b88ae9419d41fdadc05498e2b08215c3d66e3617
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-09-30 13:35:43 -0400

    wappler self updates

--- dmxAppConnect/dmxBootstrap5Tooltips/dmxBootstrap5Tooltips.js
+++ dmxAppConnect/dmxBootstrap5Tooltips/dmxBootstrap5Tooltips.js
@@ -12,9 +12,9 @@ dmx.Attribute('bs-tooltip', 'mounted', function(node, attr) {
     });
   }
 
-  this.addEventListener('destroy', () => {
+  return () => {
     if (tooltip) {
       tooltip.dispose();
     }
-  });
+  };
 });
