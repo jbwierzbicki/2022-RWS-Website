90c8e6408d08fc14af13570c6c36a7a9665fce88
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-10-07 18:37:12 -0400

    attempt 2 fix publish

--- testimonials.php
+++ testimonials.php
@@ -38,6 +38,8 @@
     <script src="dmxAppConnect/dmxDatastore/dmxDatastore.js" defer></script>
     <script src="https://www.google.com/recaptcha/api.js"></script>
 
+    <link href="https://fonts.cdnfonts.com/css/street-ruler" rel="stylesheet">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
