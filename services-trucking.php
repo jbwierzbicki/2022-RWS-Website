4ab57d864f85a2ff2194d6005b64ae4b680f8535
rjrsand <99208059+rjrsand@users.noreply.github.com>
2025-10-23 16:58:13 -0400

    Revert "removed/was converted to dropdown"
    This reverts commit 1c84d5deb1843739ccc13dcebc3bc487a522dbb5.

---
+++ services-trucking.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Freight Logistics: Trucking Services | Raging Wolf Solutions</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer=""></script>
+    <script src="dmxAppConnect/dmxBackgroundVideo/dmxBackgroundVideo.js" defer=""></script>
+
+    <link rel="stylesheet" href="css/stylesheet.css">
+
+    <link rel="icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="dmxAppConnect/dmxSlideshow/dmxSlideshow.css" />
+    <script src="dmxAppConnect/dmxSlideshow/dmxSlideshow.js" defer></script>
+    <link rel="stylesheet" href="dmxAppConnect/dmxSlideshow/themes/minimal.css" />
+    <script src="dmxAppConnect/dmxBrowser/dmxBrowser.js" defer></script>
+    <!-- Meta Pixel Code -->
+    <script>
+        function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"663126372a6e2c01c71a0faf"})},
+document.head.appendChild(o)}initApollo();
+    </script>
+    <script>
+        !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '990687025454946');
+                fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=990687025454946&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "mrz6iftq48");
+    </script>
+    <script>
+        !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_8f6e0e1be3ac9f1e37dea684410f7c092849/sdk.js"),(document.body || document.head).appendChild(n)}();
+    </script>
+    <script src="dmxAppConnect/dmxSwiper/dmxSwiper.js" defer></script>
+    <link rel="stylesheet" href="dmxAppConnect/dmxSwiper/swiper.min.css" />
+    <script src="dmxAppConnect/dmxSwiper/swiper.min.js" defer></script>
+</head>
+
+<body is="dmx-app" class="bg-dark">
+
+    <?php include 'navbar_mini'; ?>
+
+    <?php include 'navbar_main'; ?>
+
+    <main class="index-main services-main" id="servicesmaintrucking">
+        <div class="services-container wappler-block d-flex align-items-center services--main--section--container ms-0 me-0 container-fluid">
+            <div class="row services--main--section--container--row w-100 justify-content-center g-0">
+                <div class="col">
+                    <div class="row justify-content-center gx-5">
+                        <div class="text-sm-left text-left text-md-center services--main--section--container--row--col col-md-auto col-12 pt-3">
+                            <img src="assets/images/services-trucking-icon.webp" class="services--main--section--container--row--col--img rounded-circle mb-3" alt="">
+                        </div>
+                        <div class="services--main--section--container--row--col2 col-auto col-12 pt-3 pb-3 col-md-5">
+                            <h5 class="services--main--section--container--row--col2-subtitle">Our services include</h5>
+                            <h1 class="mt-0 fw-bold text-white">Standard Trucking</h1>
+                            <h5 class="mt-0 fw-bold text-light">Our team provides your company with a high capacity network that becomes a seamless extension of your supply chain.</h5>
+                            <h5 class="services--main--section--container--row--col2-h5 text-light mb-4">
+                                <i class="fas fa-angle-right"></i>&ensp;Dry Vans<br>
+                                <i class="fas fa-angle-right"></i>&ensp;Flatbeds<br>
+                                <i class="fas fa-angle-right"></i>&ensp;Conestogas<br>
+                                <i class="fas fa-angle-right"></i>&ensp;Drop-Decks<br>
+                                <i class="fas fa-angle-right"></i>&ensp;Enclosed Trailers<br>
+                                <i class="fas fa-angle-right"></i>&ensp;Curtainside Trailers
+                            </h5>
+                            <?php include 'component--services--buttongroup.php';?>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </main>
+
+    <?php include 'section_modules.php'; ?>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
